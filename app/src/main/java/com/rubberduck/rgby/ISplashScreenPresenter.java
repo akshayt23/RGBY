a15e58f707dbc7d2e3afb25bb04ce5921e37c53e
Akshay Thapa <akshay.thapa23@gmail.com>
2016-03-20 18:54:57 +0530

    Added Dummy Splash Screen

---
+++ app/src/main/java/com/rubberduck/rgby/ISplashScreenPresenter.java
@@ -0,0 +1,11 @@
+package com.rubberduck.rgby;
+
+/**
+ * Created by akshay on 3/20/16.
+ */
+public interface ISplashScreenPresenter {
+
+    public void onStart();
+
+    public void onStartButtonClicked();
+}
---
+++ app/src/main/java/com/rubberduck/rgby/ISplashScreenView.java
@@ -0,0 +1,15 @@
+package com.rubberduck.rgby;
+
+/**
+ * Created by akshay on 3/20/16.
+ */
+public interface ISplashScreenView {
+
+    void showStartGameButton();
+
+    void openGamePlayActivity();
+
+    void showProgressBar();
+
+    void hideProgressBar();
+}
